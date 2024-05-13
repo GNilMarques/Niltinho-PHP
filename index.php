2c3d6ac95f7ac3b44d598720b9493d73020046ef
GNilMarques <niltinhoaxb@gmail.com>
2024-05-13 14:46:55 -0300

    primeiro projeto

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+<?php
+function somar($nota1,$nota2,$nota3){
+    $total = $nota1+$nota2+$nota3;
+    return $total;
+}
+
+echo(somar(9,10,6));
+somar(8,7,10);
+
+
+$nome = "Olá Mundo!";
+echo "<h1>$nome</h1>";
+$x =5;
+$y =9;
+$a =20;
+$resultado =$x + $y + $a;
+echo $resultado;
+    ?>
+<p>viver não é facil não!</p><br>
+<p>viver não é facil não!</p><br>
+<p>viver não é facil não!</p><br>
+
+    <?php
+$y =10;
+echo $y;
+
+?>
+
+
+    
+</body>
+</html>
