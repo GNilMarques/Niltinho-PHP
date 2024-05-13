cfca1413231d0f7beef372b030fa50aec5e6c42b
GNilMarques <niltinhoaxb@gmail.com>
2024-05-13 15:42:26 -0300

    arquivo java script

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/stayle.css">
+
 </head>
 <body>
    
@@ -38,6 +39,6 @@ echo $y;
 <h1>niltinho</h1>
 
 
-    
+    <script src="js/script.js"></script>
 </body>
 </html>
