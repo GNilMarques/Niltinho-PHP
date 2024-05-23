6ef99b7d6c4b3df2a981a868ad624552acde26dd
GNilMarques <niltinhoaxb@gmail.com>
2024-05-23 13:15:52 -0300

    CRIAR E APAGAR PASTAS PHP

--- aula.php
+++ aula.php
@@ -1,6 +1,6 @@
 <?php
 //PASTA ARQUIVO,ALUNOS,IMAGENS,DOCUMENTOS
-    $pasta = "arquivos/aulas/imagens/documentos";
+   /* $pasta = "arquivos/aulas/imagens/documentos";
     if(!is_dir($pasta)){
         mkdir($pasta,0755,true);
     } else {
@@ -13,7 +13,7 @@
         mkdir($pasta,0755,true);
     }else {
         echo "pasta patins existente";
-    } 
+    } */
     
 //ARQUIVOS DE PASTAS
     $pasta = "arquivos/";
@@ -21,6 +21,7 @@
         mkdir($pasta,0755);
     }
 
+//criar pastas
     $nomeArquivo = date('y-m-d-H-i-s').".txt";
     $arquivo = fopen($pasta.$nomeArquivo,'a+');
     $dataAtual = date('d/m/Y');
@@ -29,12 +30,28 @@
     fwrite($arquivo,'outro aquivo de texto'.PHP_EOL);
     fwrite($arquivo,'criando mais um texto'.PHP_EOL);
     fclose($arquivo);
-
+    echo "pasta criada";
+/*
     $caminhoArquivo = $pasta.$nomeArquivo;
     if(file_exists($caminhoArquivo) && is_file($caminhoArquivo)){
         $abrirArquivo = fopen($caminhoArquivo,'r');
-        while (!feof($abrirArquivo)){
+         while (!feof($abrirArquivo)){
             echo fgets($abrirArquivo)."<br>";
-        }
+         }  
     }
+
+//apagar pastas
+    */
+    if(is_dir($pasta)){
+        foreach(scandir($pasta) as $lixo){
+            $caminho = $pasta.$lixo;
+            if(is_file($caminho)){
+                unlink($caminho);
+                echo "pasta deletada";
+            }
+        }
+        rmdir($pasta);
+    }
+         
+    
 ?>
